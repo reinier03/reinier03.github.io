56db724a921cecb86faae05669fe58b99a65ce0e
Reima <reiniermayea@gmail.com>
2024-04-01 19:56:59 -0400

    wordpress edit

---
+++ wp-content/plugins/ie-sitemode/custom-jumplist.php
@@ -0,0 +1,2 @@
+function wpcom_custom_jumplist_reader(){window.external.msSiteModeCreateJumplist("Entradas recientes");window.external.msSiteModeAddJumpListItem("\u00a1Hola mundo!","https://tutisdotcom.wordpress.com/2024/04/01/hello-world/","https://tutisdotcom.files.wordpress.com/2024/04/tutis-black-and-white.png?w=13");}
+try{if(window.external && 'msIsSiteMode' in window.external){if (window.external.msIsSiteMode()){wpcom_custom_jumplist_reader();wpcom_sitemode_comment_notify();}}}catch(e){}
